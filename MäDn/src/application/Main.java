5af3ea3f896debc1c1189f1fc4b853f0df01f11e
Jhary <jhary82@gmx.de>
2015-10-13 13:10:51 +0200

    Hinzufügen der Strategie TakefirstMoveAction

--- MäDn/src/application/Main.java
+++ MäDn/src/application/Main.java
@@ -1,15 +1,18 @@
 package application;
 
 import ludo.LudoApp;
+import strategies.TakeFirstMoveActionStrategy;
 import ludo.AbstractStrategy;
 
 public class Main {
 	public static void main(String[] args) {
-		AbstractStrategy red = new MyStrategy(); // Replace with your implementation
-		AbstractStrategy blue = new MyStrategy(); // Replace with your implementation
-		AbstractStrategy green = new MyStrategy(); // Replace with your implementation
-		AbstractStrategy orange = new MyStrategy(); // Replace with your implementation
+		AbstractStrategy red = new TakeFirstMoveActionStrategy(); // Replace with your implementation
+		AbstractStrategy blue = new TakeFirstMoveActionStrategy(); // Replace with your implementation
+		AbstractStrategy green = new TakeFirstMoveActionStrategy(); // Replace with your implementation
+		AbstractStrategy orange = new TakeFirstMoveActionStrategy(); // Replace with your implementation
 		
 		LudoApp.setStrategies(red, blue, green, orange);
 		LudoApp.launch(LudoApp.class, args);
 	}
+	
+}
---
+++ MäDn/src/strategies/TakeFirstMoveActionStrategy.java
@@ -0,0 +1,34 @@
+package strategies;
+
+import java.util.List;
+import java.util.Random;
+
+import ludo.AbstractAction;
+import ludo.AbstractStrategy;
+import ludo.MoveAction;
+import ludo.Token;
+
+/**
+ * Diese Klasse implementiert eine einfache Strategie.
+ * Es wird bei jedem Zug die erste MoveAktion der Liste der möglichen
+ * Aktionen durchgeführt. Wenn keine gefunden wurde, wird eine zufällige Aktion
+ * durchgeführt.
+ * @author Simon
+ *
+ */
+public class TakeFirstMoveActionStrategy extends AbstractStrategy {
+
+	@Override
+	public int chooseAction(List<Token> arg0, int arg1, int arg2, 
+			List<AbstractAction> arg3) {
+		
+		for(int i = 0; i < arg3.size(); i++){
+			if( arg3.get(i).getClass().equals(MoveAction.class)){
+				return i;
+			}
+		}
+								
+		return new Random().nextInt(arg3.size());
+	}
+
+}
