21c6d52fbf37c2da5056c0a81a4e0484c547ac43
Lichterkuss <erik_svenonius@gmx.de>
2015-10-13 17:37:33 +0200

    weitere strategien hinzugefügt

--- MäDn/src/application/Main.java
+++ MäDn/src/application/Main.java
@@ -9,9 +9,11 @@ public class Main {
 		AbstractStrategy red = new TakeFirstTokenStrategy(); // Replace with your implementation
 		AbstractStrategy blue = new TakeRandomActionStrategy(); // Replace with your implementation
 		AbstractStrategy green = new TakeRandomActionStrategy(); // Replace with your implementation
-		//AbstractStrategy orange = new TakeRandomActionStrategy(); // Replace with your implementation
+		AbstractStrategy orange = new TakeRandomActionStrategy();// Replace with your implementation
+		AbstractStrategy darkred = new TakeRandomActionStrategy();
+		AbstractStrategy purple = new TakeRandomActionStrategy();
 		
-		LudoApp.setStrategies(red, blue, green);
+		LudoApp.setStrategies(red, blue, green, orange, darkred, purple);
 		LudoApp.launch(LudoApp.class, args);
 	}
 	
---
+++ MäDn/src/strategies/TakeLastTokenStrategy.java
@@ -0,0 +1,39 @@
+package strategies;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import ludo.AbstractAction;
+import ludo.AbstractStrategy;
+import ludo.MoveAction;
+import ludo.Token;
+/**
+ * schlechte Strategie
+ * @author Erik
+ *
+ */
+public class TakeLastTokenStrategy extends AbstractStrategy {
+
+	@Override
+	public int chooseAction(List<Token> tokens, int turn, int die, List<AbstractAction> actions) {
+		List<MoveAction> moves = new ArrayList<>();
+		for(int i = 0; i < actions.size(); i++) {
+			if(actions.get(i).getClass().equals(MoveAction.class)) {
+				moves.add((MoveAction) actions.get(i));
+			}
+		}
+		if(moves.isEmpty()) {
+			return new Random().nextInt(actions.size());
+		} else {
+			MoveAction bPosition = moves.get(0);
+			for(MoveAction m : moves) {
+				if(m.destination().position() < bPosition.destination().position()) {
+					bPosition = m;
+				}
+			}
+			return actions.indexOf(bPosition);
+		}
+	}
+
+}
