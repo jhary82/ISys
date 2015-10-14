babbf4821fad2d44e215922dd987c86e91f2908b
Lichterkuss <erik_svenonius@gmx.de>
2015-10-14 13:13:53 +0200

    weitere auswertungen

--- MäDn/src/strategies/Strategy.java
+++ MäDn/src/strategies/Strategy.java
@@ -10,11 +10,21 @@ public abstract class Strategy extends AbstractStrategy {
 	
 	@Override
 	protected void onGameOver(List<PlayerStats> stats, int roundCount) {
+		
 		// TODO Auto-generated method stub
 		//super.onGameOver(stats, roundCount);
 		for(PlayerStats player : stats) {
 			System.out.print("Spielername: " + player.player().name() + " : ");
 			System.out.println("AnzahlGewonnen: " + player.wins());
+			
+			double win = player.wins();
+			double rc = roundCount;
+			double gh = Math.round((win/rc)*Math.pow(10, 2))/Math.pow(10, 2);
+			System.out.println("GewinnHäufigkeit: " + gh*100);
+			
+			double dg = roundCount/stats.size();
+			double dgv = ((win-dg)/dg);
+			System.out.println("Gewinne/Verluste im Durchschnitt: " + dgv*100);
 		}
 		System.out.println(" " + roundCount);
 		
