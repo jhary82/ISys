95c6ff6e760e8e1de531fa8cb00e7f08c19749ca
Lichterkuss <erik_svenonius@gmx.de>
2015-10-19 11:33:21 +0200

    auswertung bearbeitet

--- MäDn/src/strategies/Strategy.java
+++ MäDn/src/strategies/Strategy.java
@@ -12,24 +12,25 @@ public abstract class Strategy extends AbstractStrategy {
 	protected void onGameOver(List<PlayerStats> stats, int roundCount) {
 		double rc = roundCount;
 		double cp = stats.size();
-		double dg = rc/cp;
+		double dg = Math.round((rc/cp)*Math.pow(10, 2))/Math.pow(10, 2);
 		System.out.println("RundenInsgesamt: " + roundCount);
 		System.out.println("Ø gewinne pro Spieler: " + dg);
+		
+		System.out.println("------------------------------------------");
 		for(PlayerStats player : stats) {
-			System.out.println("------------------------------------------");
-			
-			System.out.print("Spielername: " + player.player().name() + " : ");
-			System.out.println("AnzahlGewonnen: " + player.wins());
 			
 			double win = player.wins();
 			
-			double gh = Math.round((win/rc)*Math.pow(10, 2))/Math.pow(10, 2);
-			System.out.println("GewinnHäufigkeit: " + gh*100);
+			System.out.print("Spielername: " + player.player().name() + " : ");
+			System.out.println("AnzahlGewonnen: " + player.wins());
+			System.out.println("Spiele gewonnen in Prozent: " + win/rc*100 + " %");
 			
-			
+			/*
 			double dgv = ((win-dg)/dg);
 			System.out.println("Gewinne/Verluste im Durchschnitt: " + dgv*100);
+			*/
 			
+			System.out.println("------------------------------------------");
 		}
 		
 		
