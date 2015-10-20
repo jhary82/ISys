97e6718a93b733562b25af9bd82dc611a8b05168
Simon Krause <simon.krause@kekkimekki.de>
2015-10-20 17:29:34 +0200

    Neue Strategie eingefügt

---
+++ MäDn/src/strategy/KillandFleeStrategy.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package strategy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import ludo.AbstractAction;
+import ludo.MoveAction;
+import ludo.Token;
+
+/**
+ * Diese Strategie versucht möglichst sichere Züge zu machen.
+ * Andere Token werden geschlagen oder gemieden.
+ * @author Simon
+ * 
+ */
+public class KillandFleeStrategy extends Strategy {
+
+	public KillandFleeStrategy(boolean evaluate, int ownIndex, String csvName) {
+		super(evaluate, ownIndex, csvName);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	@Override
+	public int chooseAction(List<Token> tokens, int turn, int die, List<AbstractAction> actions) {
+		List<MoveAction> moves = new ArrayList<>();
+		List<MoveAction> hits = new ArrayList<>();
+		
+		for (int i = 0; i < actions.size(); i++) {
+			if (actions.get(i).getClass().equals(MoveAction.class)) {
+				moves.add((MoveAction) actions.get(i));
+			}
+		}
+		if (moves.isEmpty()) {
+			return new Random().nextInt(actions.size());
+		} else {
+			
+			hits = canHit(tokens, moves);						
+			if(!hits.isEmpty()){
+				return new Random().nextInt(hits.size());
+			}
+			else{
+				List<MoveAction> inDanger = new ArrayList<>();
+				for(MoveAction move : moves){
+					
+				}
+				
+				if( inDanger.isEmpty()){
+					return sortPosition(moves)
+				}
+			}			
+		}
+		
+	}
+
+}
