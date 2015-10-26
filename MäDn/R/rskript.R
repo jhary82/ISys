e64fd15803d90cbe25bf2e7c521c32338f8e9b14
Simon Krause <simon.krause@kekkimekki.de>
2015-10-26 20:17:14 +0100

    R ordner gelöscht

--- MäDn/R/rskript.R
+++
@@ -1,9 +0,0 @@
-# R Skript
-
-#Einlesen der csv-Datei
-m1 <- read.csv2("../Strategy1.csv", header=TRUE)
-
-#Gewinn-Quote
-won <- length ( m$isWon[ m$isWon == 1 ] )
-length <- length( m$isWon )
-won / length
