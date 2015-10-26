69d1ec7f5896c98a4ea9496bf42af23a10b92735
Simon Krause <simon.krause@kekkimekki.de>
2015-10-26 17:11:32 +0100

    Statistik

---
+++ MäDn/ext/6_player_statistics/linuxSkript6.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ..
+java -cp ludo.jar:commons-cli-1.2.jar:ludo-new.jar ludo.LudoCli -m 10000 -p . -r TakeFirstBeatTokenStrategy -b  TakeRandomActionStrategy -g  TakeRandomActionStrategy -o  TakeRandomActionStrategy -v TakeRandomActionStrategy -br TakeRandomActionStrategy
+
+java -cp ludo.jar:commons-cli-1.2.jar:ludo-new.jar ludo.LudoCli -m 10000 -p . -r TFBTandKAF -b  TakeRandomActionStrategy -g  TakeRandomActionStrategy -o  TakeRandomActionStrategy -v TakeRandomActionStrategy -br TakeRandomActionStrategy
+
+java -cp ludo.jar:commons-cli-1.2.jar:ludo-new.jar ludo.LudoCli -m 10000 -p . -r KillAndFleeStrategy -b  TakeRandomActionStrategy -g  TakeRandomActionStrategy -o  TakeRandomActionStrategy -v TakeRandomActionStrategy -br TakeRandomActionStrategy
+
+
