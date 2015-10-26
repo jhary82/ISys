ebeb25c1a683493eda628278f4e4e2a580f23c30
Lichterkuss <erik_svenonius@gmx.de>
2015-10-26 12:20:40 +0100

    Strategien-Test in Doku hinzugefügt.

--- MäDn/src/application/Main.java
+++ MäDn/src/application/Main.java
@@ -6,8 +6,8 @@ import ludo.AbstractStrategy;
 
 public class Main {
 	public static void main(String[] args) {		
-		//AbstractStrategy red = new TakeFirstBeatTokenStrategy(true, 0, "TakeFirstBeatStrategy"); // Replace with your implementation
-		//AbstractStrategy red = new KillAndFleeStrategy(true, 0, "KillAndFleeStrategy"); // Replace with your implementation
+		//AbstractStrategy red = new TakeFirstBeatTokenStrategy(false, 0, "TakeFirstBeatStrategy"); // Replace with your implementation
+		//AbstractStrategy red = new KillAndFleeStrategy(false, 0, "KillAndFleeStrategy"); // Replace with your implementation
 		AbstractStrategy red = new TFBTandKAF(false, 0, "TFBTandKAF"); // Replace with your implementation
 		AbstractStrategy blue = new TakeRandomActionStrategy(); // Replace with your implementation
 		AbstractStrategy green = new TakeRandomActionStrategy(); // Replace with your implementation
@@ -15,7 +15,7 @@ public class Main {
 		AbstractStrategy darkred = new TakeRandomActionStrategy();
 		AbstractStrategy purple = new TakeRandomActionStrategy();
 		
-		LudoApp.setStrategies(red, blue, green, orange/*, darkred, purple*/);
+		LudoApp.setStrategies(red, blue, green, orange, darkred, purple);
 		LudoApp.launch(LudoApp.class, args);
 	}
 	
