8fa824601323d73251a3bb436751b7362776ee2c
Erik Svenonius <erik_svenonius@gmx.de>
2015-12-15 13:06:00 +0100

    TestKlasse weitergemacht, Probleme mit den Umlauten (ÄÜÖäüö)

--- Textklassifikation/src/syntaxAnalyse/Syntax.g4
+++ Textklassifikation/src/syntaxAnalyse/Syntax.g4
@@ -8,6 +8,11 @@ QUES: '?';
 CITE: ('>>' | '<<' | '"');
 NUMBER: DIGIT;
 NL: '\n';
-WS: [ \t];
+WS: [ \t\r] -> skip;
+WORD: LETTER -> skip;
+//TEST: UMLAUTE -> skip;
+TEST: 'ä' -> skip;
 
 fragment DIGIT: [0-9]+;
+fragment LETTER: [a-zA-Z]+;
+/*fragment UMLAUTE: ('Ä'|'Ü'|'Ö'|'ä'|'ü'|'ö')+;*/
--- Textklassifikation/src/test/Test.java
+++ Textklassifikation/src/test/Test.java
@@ -3,6 +3,13 @@
  */
 package test;
 
+import java.io.IOException;
+
+import org.antlr.v4.runtime.*;
+
+import syntaxAnalyse.Syntax;
+
+import static syntaxAnalyse.Syntax.*;
 /**
  * @author Erik
  *
@@ -13,8 +20,33 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		CharStream input = null;
+		if(args.length > 0) {
+			try {
+				input = new ANTLRFileStream(args[0]);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		Syntax lexer = new Syntax(input);
+		Token t = lexer.nextToken();
+		while(t.getType() != Token.EOF) {
+			switch (t.getType()) {
+			case DOT: break;
+			case KOM: break;
+			case AUS: break;
+			case BRACK: break;
+			case RBRACK: break;
+			case QUES: break;
+			case CITE: break;
+			case NUMBER: break;
+			case NL: break;
+			default: break;
+			}
+			//System.out.print(t.getText());
+			t = lexer.nextToken();
+		}
 	}
 
 }
