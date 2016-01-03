5f12df3275cc6b0ec9851f0f137f0a3afde2a00f
Simon Krause <simon.krause@kekkimekki.de>
2016-01-03 01:40:30 +0100

    neue Rohdaten erzeugt

--- Textklassifikation/Auswertung/drawPlots.R
+++ Textklassifikation/Auswertung/drawPlots.R
@@ -6,17 +6,15 @@ args <- commandArgs(TRUE)
 evaluate <- function(m) {
 	hist( m$DOT, main="Anzahl Punkte" )
 	hist( m$COMMA, main="Anzahl Kommata")
-	hist( m$EXCLAMATION, main="Anzahl Ausrufezeichen" )
-	hist( m$QUESTION, main="Anzahl Fragezeichen")
+	hist( m$QUESTION, main="Anzahl ! und ?")
 	hist( m$CITE, main="Anzahl Zitate" )
-	hist( m$WORDS, main="Anzahl der Wörter" )
-	hist( m$SENTENCE_LENGTH, main="Satzlängen" )
-	hist( m$PAST, main="Vergangenheitsendungen" )
+	hist( m$SENTENCE_LENGTH_AVG, main="durchschnittliche Satzlängen" )
 	hist( m$NUMBER_WITH_DOT, main="Zahlen mit Punkten enthalten" )
 	hist( m$NL, main="Anzahl von nl" )
 	hist( m$NUMBER_FOUR, main="vierstellige Zahlen(Jahreszahlen)" )
 	hist( m$NUMBER_REST, main="restliche Zahlen" )
 	hist( m$BRACK, main="Anzahl an Klammern")
+	hist( m$SUB_SENTENCES, main="Anzahl an Haupt-,Nebensätzen")
 }
 
 # lade alle Dateien ein
